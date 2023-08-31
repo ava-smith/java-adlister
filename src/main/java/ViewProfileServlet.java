f96ac813e5844242b210fda011c537e899a303c4
Ava Smith <ava013005@gmail.com>
2023-08-31 11:10:53 -0500

    finished walkthrough

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -9,8 +9,7 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        if(session.getAttribute("user") == null) {
+        if(request.getSession().getAttribute("user") == null) {
             response.sendRedirect("/login");
         } else {
             request.getRequestDispatcher("WEB-INF/profile.jsp").forward(request, response);
